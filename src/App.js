7dea75f71760e8713b19b69973f836290c8f29de
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-24 15:17:29 -0600

    Add form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Working
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // constructor
+  constructor (props) {
+    super(props);
+    // add state
+    this.state = [];
+  }
+
+  // add instance methods. use arrow functions to maintain "this" context.
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submit!')
+  }
+
+
+  // render
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>City Explorer</h1>
+        </header>
+        <form onSubmit={this.handleSubmit}>
+            <button type='submit'>Display Stuff</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
