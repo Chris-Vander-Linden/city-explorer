f52939b472c54b3e590dec27fc30e6da065e58d1
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-24 22:21:18 -0600

    Add city search results

--- src/App.js
+++ src/App.js
@@ -1,41 +1,82 @@
 import React from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import './App.css';
 
-// const API_KEY = process.env.REACT_APP_LOCATION_KEY;
+const API_KEY = process.env.REACT_APP_LOCATION_KEY;
 
 class App extends React.Component {
   // constructor
   constructor (props) {
     super(props);
     // add state
-    this.state = {results: []};
+    this.state = {
+      city: '',
+      results: [],
+      error: false
+    };
   }
 
-  // add instance methods. use arrow functions to maintain "this" context.
+  handleFormChange = (event) => {
+    this.setState({city: event.target.value}) 
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
 
-    axios.get('https://swapi.dev/api/people/?page=1').then(response => {
-      console.log(response.data.results);
-      this.setState({results: response.data.results}) 
+    // if there is already a result, then the form resets on submit
+    this.state.results.length === 1 ? this.setState({results: [], city: ''}) : 
+    
+    // else if filter for city
+    this.state.results.length > 1 ? this.setState({results: this.state.results.filter(result => result.display_name === this.state.city
+)}) :
+
+    // else fetch data from API
+    axios.get(`https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.city}&format=json`).then(response => {
+      console.log(response.data);
+      this.setState({results: response.data}); 
+      }).catch(error => {
+        console.log('ERRRRRRR')
+        console.error(error);
       });
   }
 
-  // render
   render() {
-    // create list of results
-    const resultList = this.state.results.map((result, indx) => <li key={indx}>{result.name}</li>)
+    // create list of results.
+    const resultList = this.state.results.map(result => <option key={result.place_id
+}>{result.display_name}</option>)
 
     return (
-      <div>
+      <div id='container'>
         <header>
           <h1>City Explorer</h1>
         </header>
-        <form onSubmit={this.handleSubmit}>
-            <button type='submit'>Display Stuff</button>
-        </form>
-        <ul>{resultList}</ul>
+      <br />
+    <Form onSubmit={this.handleSubmit}>
+
+      {this.state.results.length === 0 && <Form.Group className="mb-3" controlId="formSearchCity">
+        <Form.Label>City: </Form.Label>
+        <Form.Control type="text" placeholder="Type city or zip code..." name='city' value={this.state.city} onChange={this.handleFormChange}/>
+      </Form.Group>}
+
+      {this.state.results.length > 1 && <Form.Group className="mb-3" controlId="formSelectCity">
+        <Form.Label>Which City? </Form.Label>
+        <Form.Select aria-label="Select City" value={this.state.city} onChange={this.handleFormChange}>
+          <option key="-">Select City...</option>
+          {resultList}
+        </Form.Select>
+      </Form.Group>}
+
+      <Button variant="primary" type="submit">{this.state.results.length === 1 ? 'Search Again?' : this.state.results.length === 0 ? 'Explore!' : 'Search Cities.'}</Button>
+
+    </Form>
+
+      {this.state.results.length === 1 && <><h2>Results:</h2>
+      Name: {this.state.results[0]?.display_name} <br />
+      Lat: {this.state.results[0]?.lat}<br />
+      Lon: {this.state.results[0]?.lon}<br /></>}
       </div>
     );
   }
