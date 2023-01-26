a1d756f5e6c75351950c55880a8263851ef2fb58
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-25 20:19:04 -0600

    Add maplibre-gl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import Header from './Header.js';
+import Map from './Map.js';
+import MapForm from './MapForm.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const API_KEY = process.env.REACT_APP_LOCATION_KEY;
@@ -43,48 +44,12 @@ class App extends React.Component {
   }
 
   render() {
-    // create list of results.
-    const resultList = this.state.results.map(result => <option key={result.place_id
-}>{result.display_name}</option>);
-
     return (
       <div id='container'>
-        <header>
-          <h1>City Explorer</h1>
-        </header>
-      <br />
-    <Form onSubmit={this.handleSubmit}>
-
-      {/* search for city by zip or name */}
-      {this.state.results.length === 0 && <Form.Group className="mb-3" controlId="formSearchCity">
-        <Form.Label>City: </Form.Label>
-        <Form.Control type="text" placeholder="Type city or zip code..." name='city' value={this.state.city} onChange={this.handleFormChange}/>
-      </Form.Group>}
-
-      {/* if more than 1 city, then create select to narrow result to just 1 */}
-      {this.state.results.length > 1 && <Form.Group className="mb-3" controlId="formSelectCity">
-        <Form.Label>Which City? </Form.Label>
-        <Form.Select aria-label="Select City" value={this.state.city} onChange={this.handleFormChange}>
-          <option key="-">Select City...</option>
-          {resultList}
-        </Form.Select>
-      </Form.Group>}
-
-      {/* change button text based on condition of form */}
-      <Button variant="primary" type="submit">{this.state.results.length === 1 ? 'Search Again?' : this.state.results.length === 0 ? 'Explore!' : 'Search Cities.'}</Button>
-
-    </Form>
-
-      {/* display single city result */}
-      {this.state.results.length === 1 && <><h2>Results:</h2>
-      Name: {this.state.results[0]?.display_name} <br />
-      Lat: {this.state.results[0]?.lat}<br />
-      Lon: {this.state.results[0]?.lon}<br /></>}
-
-      {/* display map */}
-      {this.state.results.length === 1 && <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${this.state.results[0]?.lat},${this.state.results[0]?.lon}&zoom= 1-18`} alt={this.state.results[0]?.display_name} />}
-
-
+        <Header />
+        <MapForm results={this.state.results} onHandleFormChange={this.handleFormChange} onHandleSubmit={this.handleSubmit}/>
+        <Map results={this.state.results}/>
+        <footer>&copy; Chris Vander Linden {new Date().getFullYear()}</footer>
       </div>
     );
   }
